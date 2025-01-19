93628d027e99c6183a995c4e25c0b070aea9dfed
Федор Лукашов <fiodorlukashov@gmail.com>
2025-01-19 10:04:24 -0800

    Update compile script

--- compile.bat
+++ compile.bat
@@ -1,11 +1,19 @@
 @echo off
-set SCRIPT=your_script.py
+
+set VENV_PATH=venv
+set TEMP_DIR=temp
+
+call %VENV_PATH%\Scripts\activate.bat
+
+set SCRIPT=main.py
 set OUTPUT_DIR=dist
 
-nuitka --standalone --onefile --mingw64 --output-dir=%OUTPUT_DIR% %SCRIPT%
+nuitka --standalone --onefile --mingw64 --output-dir=%OUTPUT_DIR% --lto=yes --nofollow-import-to=unittest --nofollow-import-to=test --onefile-tempdir-spec="%TEMP_DIR%\\%RANDOM%_%PID%" %SCRIPT%
 
 if %ERRORLEVEL% equ 0 (
     echo Компиляция успешно завершена. Исполняемый файл находится в папке %OUTPUT_DIR%.
 ) else (
     echo Ошибка при компиляции.
 )
+
+deactivate
