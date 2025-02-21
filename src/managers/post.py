09ac4d7e00b8ee4d8b33b768d48e2696b375c98d
Fyodor <fiodorlukashov@gmail.com>
2025-02-21 19:59:08 +0200

    clip_upload with asyncio #1

--- src/managers/post.py
+++ src/managers/post.py
@@ -1,3 +1,4 @@
+import asyncio
 from instagrapi import Client
 from instagrapi.exceptions import LoginRequired, ChallengeRequired
 from src.tools import console
@@ -9,12 +10,14 @@ class PostManager:
 
     async def post_video(self, video_path: str, description: str) -> bool:
         try:
-            self.client.clip_upload(
-                video_path,
-                caption=description,
-                thumbnail=None,
-                location=None,
-                extra_data={}
+            asyncio.to_thread(
+                self.client.clip_upload(
+                    video_path,
+                    caption=description,
+                    thumbnail=None,
+                    location=None,
+                    extra_data={}
+                )
             )
             console.print(f"[green]Видео загружено | Путь: {video_path}[/]")
             return True
